c35d7597ad64dce64231b1b292dac2c809fe3e78
Gustavo Mardones <gustavo.mardones@hotmail.cl>
2023-08-24 02:35:19 -0400

    modificando css y agregando modal2 con video

--- components/sections/Banner1.js
+++ components/sections/Banner1.js
@@ -2,7 +2,8 @@ import { useState } from "react"
 import YouTubeVideo from "../tools/YoutubeAPI"
 
 export default function Banner1() {
-    const videoId = '06bxyMCfdPA'
+    const videoId1 = '06bxyMCfdPA'
+    const videoId2 = '8CC7f0Hr2cc'
 
     const [modalClick1, setModal1] = useState(false)
     const [modalClick2, setModal2] = useState(false)
@@ -15,25 +16,38 @@ export default function Banner1() {
             <section className="banner-area">
                 <div className="container">
                     <div className="row">
-                        <h3>En Magavi ofrecemos soluciones reales</h3>
                         <div className="col-lg-12">
-                            <button  onClick={handleModlaClick2} className="gradient-btn wow fadeInLeft"> Innovación Digital</button>
-                            <button onClick={handleModlaClick1} className="gradient-btn gradient-btn-two wow fadeInRight">Ciberseguridad</button>
+                            <div className="banner1-title-button">
+                                <h3>En Magavi ofrecemos soluciones reales</h3>
+                                <button  onClick={handleModlaClick2} className="gradient-btn wow fadeInLeft"> Innovación Digital</button>
+                                <button onClick={handleModlaClick1} className="gradient-btn gradient-btn-two wow fadeInRight">Ciberseguridad</button>
+                            </div>
+                            <dialog className="dialog" open={modalClick1}>
+                                <div className="dialog-container">
+                                    <div className="dialog-right-container">
+                                        <YouTubeVideo videoId={videoId2} width={"100%"} height={"350vh"}/>
+                                    </div>
 
-                            {/* <dialog className="dialog-container" open={modalClick1}>
-                                <div className="dialog-right-container"></div>
-
-                                <div className="dialog-left-containers">
-                                    <div class="dialog-top-left-container"></div>
-                                    <div class="dialog-bottom-left-container"></div>
+                                    <div className="dialog-left-containers">
+                                        <div class="dialog-top-left-container">
+                                            <h1>ASESORIA EN <span className="dialog-span-gradient">CIBERSEGURIDAD</span></h1>
+                                        </div>
+                                        <div class="dialog-bottom-left-container">
+                                            <p>¿Tus datos están protegidos? Nuestro equipo de expertos en ciberseguridad y protección de 
+                                                datos están aquí para brindarle asesoría personalizada. Garantice la seguridad de su información.</p>
+                                        </div>
+                                    </div>
                                 </div>
-                                
-                            </dialog> */}
+                                <div className="dialog-buttons">
+                                    <button id="dialog-close-btn" onClick={handleModlaClick1}>Cerrar</button>
+                                    <button id="dialog-chat-btn">¿Conversamos?</button>
+                                </div>
+                            </dialog>
 
                             <dialog className="dialog" open={modalClick2}>
                                 <div className="dialog-container">
                                     <div className="dialog-right-container">
-                                        <YouTubeVideo videoId={videoId} width={"100%"} height={"300vh"}/>
+                                        <YouTubeVideo videoId={videoId1} width={"100%"} height={"350vh"}/>
                                     </div>
 
                                     <div className="dialog-left-containers">
